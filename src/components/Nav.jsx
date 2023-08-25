b73df437d12171951d4ea096b037cc9c9357a350
lccarrillo247 <lccarrillo247@gmail.com>
2023-08-25 11:31:17 -0500

    Botón random

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,7 +1,17 @@
 import SearchBar from "./SearchBar";
 
 export default function Nav(props) {
+
+    const aleatorio = (max) => {
+        return Math.floor(Math.random() * max);
+    }
+
     return (
+        <>
         <SearchBar onSearch={props.onSearch} />
+        <button
+        onClick={() => props.onSearch(aleatorio(826))}
+        >Random</button>
+        </>
     )
 }
