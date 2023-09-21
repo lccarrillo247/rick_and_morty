fa9649335216cafc82730852c887cb27cb4ed4c7
lccarrillo247 <lccarrillo247@gmail.com>
2023-09-21 17:22:35 -0500

    03-sequelize Integration 01

--- Server/src/index.js
+++ Server/src/index.js
@@ -1,9 +1,11 @@
 // EXPRESS
 const app = require("./app")
+const { conn } = require('./DB_connection')
 
 const PORT = 3001;
 
 app.listen(PORT, () => {
+    conn.sync({force: true});
     console.log('Server raised in port: ' + PORT);
 });
 
